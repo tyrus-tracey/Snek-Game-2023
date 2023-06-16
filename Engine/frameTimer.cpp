8f84236290aa0f22c055c452fffaa728b43c3830
tyrus-tracey <ttracey@sfu.ca>
2023-06-16 11:19:45 +0900

    added frametimer class

---
+++ Engine/frameTimer.cpp
@@ -0,0 +1,21 @@
+#include "frameTimer.h"
+
+frameTimer::frameTimer()
+{
+	updateTimePoint();
+}
+
+void frameTimer::updateTimePoint()
+{
+	last = std::chrono::steady_clock::now();
+}
+
+float frameTimer::getDiff()
+{
+	std::chrono::duration<float> output = std::chrono::steady_clock::now() - last;
+	updateTimePoint();
+	return output.count();
+}
+
+
+
---
+++ Engine/frameTimer.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <chrono>
+
+class frameTimer
+{
+public:
+	frameTimer();
+	float getDiff();
+private:
+	std::chrono::steady_clock::time_point last;
+	void updateTimePoint();
+};
+
